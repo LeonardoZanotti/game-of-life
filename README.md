2c2cd7283f3f3ac70f9e0374abdc588ef4ce8438
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-04-21 16:52:26 -0300

    Made some part of the README.md and several adds to the game, added a template of the Gosper glider gun and generating random boards

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
-# Game of life
+![](https://upload.wikimedia.org/wikipedia/commons/e/e5/Gospers_glider_gun.gif)
 
-https://www.geeksforgeeks.org/conways-game-life-python-implementation/
-https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
-python3.7 -m pip install numpy matplotlib
+# Game of life (Conways)
+The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970. It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves. It is Turing complete and can simulate a universal constructor or any other Turing machine. 
 
-https://numpy.org/doc/1.20/
-https://matplotlib.org/stable/contents.html#
+## Running this game
+First of all, you will need [Python 3.7+](https://www.python.org/).
+With this, is needed to install the requirements of this project: [Numpy](https://numpy.org) and [Matplotlib](https://matplotlib.org/). To install it, just run:
+```bash
+$ python -m pip install numpy matplotlib
+```
+All installed, you can run the project helper with:
+```bash
+$ python gameoflife.py --help
+```
+This will give you an example of how to use the project.
+
+Basically, you can run the project in a random mode, generating a NxN board with a random start configuration or you can use "--template" to see the [Gosper glider gun](https://conwaylife.com/wiki/Gosper_glider_gun).
+
+## Acknowledgment
+Thanks to this [Geeks for Geeks article](https://www.geeksforgeeks.org/conways-game-life-python-implementation/), improved by ganesh_kavhar. It give me the inspiration to do my own version of the game.
+
+## Leonardo Zanotti
--- gameoflife.py
+++ gameoflife.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 # Leonardo José Zanotti
-# https://github.com/LeonardoZanotti/Zawnbe
-
-# pytube documentation if you are reading this to learn :D
-# https://readthedocs.org/projects/python-pytube/downloads/pdf/latest/
+# https://github.com/LeonardoZanotti/game-of-life
 
 # -*- coding: utf-8 -*-
 import sys,os,platform
@@ -21,85 +18,131 @@ colors = True	# output colored c:
 machine = sys.platform 		# detecting the os
 checkPlatform = platform.platform()	# get current version of os
 if machine.lower().startswith(('os', 'win', 'darwin', 'ios')):
-	colors = False 	# Mac and Windows shouldn't display colors :c
+    colors = False 	# Mac and Windows shouldn't display colors :c
 if checkPlatform.startswith("Windows-10") and int(platform.version().split(".")[2]) >= 10586:
-	color = True	# coooolorssss \o/
-	os.system('')	# Enables the ANSI -> standard encoding that reads that colors
+    color = True	# coooolorssss \o/
+    os.system('')	# Enables the ANSI -> standard encoding that reads that colors
 if not colors:
-	BGreen = BYellow = BPurple = BCyan = Yellow = Green = Red = Blue = On_Black = ''
+    BGreen = BYellow = BPurple = BCyan = Yellow = Green = Red = Blue = On_Black = ''
 else:
-	BGreen = "\033[1;32m"     # Bold Green
-	BYellow = "\033[1;33m"    # Bold Yellow
-	BPurple = "\033[1;35m"    # Bold Purple
-	BCyan = "\033[1;36m"      # Bold Cyan
-	Yellow = "\033[0;33m"     # Yellow
-	Green = "\033[0;32m"      # Green
-	Red = "\033[0;31m"      # Red
-	Blue = "\033[0;34m"     # Blue
+    BGreen = "\033[1;32m"     # Bold Green
+    BYellow = "\033[1;33m"    # Bold Yellow
+    BPurple = "\033[1;35m"    # Bold Purple
+    BCyan = "\033[1;36m"      # Bold Cyan
+    Yellow = "\033[0;33m"     # Yellow
+    Green = "\033[0;32m"      # Green
+    Red = "\033[0;1"      # Red
+    Blue = "\033[0;34m"     # Blue
 
-	# Background
-	On_Black="\033[40m"       # Black Background
+    # Background
+    On_Black="\033[40m"       # Black Background
 
 ##### logo
 if len(args) == 1:		### Made with figlet or toilet
-	print('''
-		{On_Black}{BCyan}
+    print('''
+        {On_Black}{BCyan}
         \t  ____                               __   _     _  __      
         \t / ___| __ _ _ __ ___   ___    ___  / _| | |   (_)/ _| ___ 
         \t| |  _ / _` | '_ ` _ \ / _ \  / _ \| |_  | |   | | |_ / _ \\
         \t| |_| | (_| | | | | | |  __/ | (_) |  _| | |___| |  _|  __/
         \t \____|\__,_|_| |_| |_|\___|  \___/|_|   |_____|_|_|  \___|
-		\t
-		{BYellow} # Zanotti's python Game of life{Green}
-		https://github.com/LeonardoZanotti/game-of-life
+        \t
+        {BYellow} # Zanotti's python Game of life{Green}
+        https://github.com/LeonardoZanotti/game-of-life
 
 For {BGreen}help {Green}type:
 \t
 {BGreen}$ python3.7 gameoflife.py --help                             
-		'''.format(BCyan=BCyan,BGreen=BGreen,Green=Green,BYellow=BYellow,On_Black=On_Black))                                   
+        '''.format(BCyan=BCyan,BGreen=BGreen,Green=Green,BYellow=BYellow,On_Black=On_Black))                                   
 
-	sys.exit()
+    sys.exit()
 
 ##### options help
 def helper():
-	print('''
-{BGreen}Usage: gameoflife.py [size of the canvas] [cells initially alive]
+    print('''
+{BGreen}Usage: gameoflife.py [size of the canvas]
 
-Example: python3.7 gameoflife.py 255 1 2 3 4 8 9
-This will create a canvas of 255x255 cells with the cells 1, 2, 3, 4, 8 and 9 initially alive
-	'''.format(BGreen=BGreen))
-	return
+Example: python3.7 gameoflife.py 255
+This will create a canvas of 255x255 random cells
+
+Example: python3.7 gameoflife.py 255 --template
+This will create a canvas of 255x255 cells from a defined template
+    '''.format(BGreen=BGreen))
+    return
 
 ##### options list
 parser = OptionParser(add_help_option=False)
 parser.add_option('-h','--help',action='store_true',dest='help')
+parser.add_option('-t','--template',action='store_true',dest='template')
 opts, args = parser.parse_args()
 
 ##### analysing options
 if (opts.help or len(args) <= 1):
-	helper()
+    helper()
 
-def make_checkerboard(n_rows, n_columns, square_size):
-    rows_grid, columns_grid = np.meshgrid(range(n_rows), range(n_columns), indexing='ij')
-    print(rows_grid)
-    print(columns_grid)
-    high_res_checkerboard = np.mod(rows_grid, 2) + np.mod(columns_grid, 2) == 1
+def make_checkerboard(n_rows, n_columns, square_size, random):
+    print('Making checkearboard...')
+    new_rows = n_rows if random else 21
+    new_columns = n_columns if random else 49
+    if (random):
+        rows_grid, columns_grid = np.meshgrid(range(new_rows), range(new_columns), indexing='ij')
+        print('Grids done')
+        print('Rows:')
+        print(rows_grid)
+        print('Colums')
+        print(columns_grid)
+
+        r = new_rows
+        c = new_columns
+        p = 0.4
+        high_res_checkerboard = np.random.choice(a=[False, True], size=(r, c), p=[p, 1-p])
+    else:
+        template = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+        print('Template:')
+        print(template)
+        high_res_checkerboard = np.array(template, dtype=bool)
+
+    print('Checkeds:')
     print(high_res_checkerboard)
+
     square = np.ones((square_size,square_size))
+    print('Square size:')
     print(square)
-    checkerboard = np.kron(high_res_checkerboard, square)[:n_rows,:n_columns]
+
+    checkerboard = np.kron(high_res_checkerboard, square)[:new_rows,:new_columns]
+    print('Checkerboard:')
+    print(checkerboard)
+
     return checkerboard
 
-if (args[0]):
-    square_size = 5
-    n_rows = 14
-    n_columns = 67
-    checkerboard = make_checkerboard(n_rows, n_columns, square_size)
-    print(checkerboard)
+if len(args) > 0:
+    square_size = 1
+    n_rows = n_columns = int(args[0])
+    random = False if opts.template else True
+    checkerboard = make_checkerboard(n_rows, n_columns, square_size, random)
     plt.imshow(checkerboard)
     plt.show()
-
-if len(args) > 1:
-    print('dale')
 
 sys.exit()
